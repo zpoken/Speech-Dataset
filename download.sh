de50cf36a6f15bcd7a67d3e9e02ed84f502017d0
Makar Troyan <makar.troyan@gmail.com>
2021-05-19 06:53:38 +0300

    init commit

---
+++ download.sh
@@ -0,0 +1,5 @@
+gdown https://drive.google.com/uc?id=1DiAXSK1DQf2beiN0-aJ3nDPK_f_5-S4C
+gdown https://drive.google.com/uc?id=1GRDrZjjOTfO5WohDX6Q83PgI5VMOyNlm
+gdown https://drive.google.com/uc?id=1FJhT_U4FHZaz5bosEAlfy-77dYn0qir5
+gdown https://drive.google.com/uc?id=1qEsOFMWxFZ4mSvEul2RuaY8JRq075bjZ
+gdown https://drive.google.com/uc?id=1Ko5WdTvaxME7XP2k6pNjttoc7dgL4I-x
